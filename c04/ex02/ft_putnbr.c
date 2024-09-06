80b1f9e79f2ddef5f6df57a0ed925e5a27d68a22
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-06 01:35:26 -0300

    add c04

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 22:35:21 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/06 01:31:34 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unistd.h"
+
+void	ft_putnbr(int nbr)
+{
+	char	*base;
+
+	base = "0123456789";
+	if (nbr == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	else if (nbr < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(nbr *= -1);
+		return ;
+	}
+	else if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	write(1, &base[nbr % 10], 1);
+}
+/*
+int main(void)
+{
+	ft_putnbr(62931);
+}*/
---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 01:32:58 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/06 01:35:02 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+#include <stdio.h>
+
+int	ft_atoi(char *str);
+
+int	main(void)
+{
+	char str1[] = " ---+--+1234ab567";
+	char str2[] = "   +42";
+	char str3[] = "   -+-+---512";
+	char str4[] = "   2147483647";
+	char str5[] = "   -2147483648";
+
+	printf("Resultado 1: %d\n", ft_atoi(str1));  // Deve imprimir -1234
+	printf("Resultado 2: %d\n", ft_atoi(str2));  // Deve imprimir 42
+	printf("Resultado 3: %d\n", ft_atoi(str3));  // Deve imprimir -512
+	printf("Resultado 4: %d\n", ft_atoi(str4));  // Deve imprimir 2147483647
+	printf("Resultado 5: %d\n", ft_atoi(str5));  // Deve imprimir -2147483648
+
+	return (0);
+}/**/
