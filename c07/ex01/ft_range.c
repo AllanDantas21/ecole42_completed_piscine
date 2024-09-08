23327d281254103c2da027e0eaef64cb7a6ce7d4
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-08 01:42:27 -0300

    add ft_range

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,13 +6,40 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 01:19:04 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/08 01:23:40 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/08 01:41:28 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Escreva uma função ft_range que inverta um array de int. Esse array de int
-// deverá conter todos os valores entre min e max.
-// • Min incluído - max excluído.
-// • Ela deverá ser prototipada da seguinte maneira:
-// int *ft_range(int min, int max);
-// • Se o valor min for superior ou igual ao valor max, um ponteiro nulo será retornado
+#include <stdlib.h>
+
+int *ft_range(int min, int max)
+{
+    int *range;
+    int i;
+
+    if (min >= max)
+        return (0);
+    range = malloc(sizeof(int) * (max - min));
+    if (!range)
+        return (0);
+    i = 0;
+    while (min < max)
+        range[i++] = min++;
+    return (range);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int *range;
+//     int i;
+
+//     range = ft_range(1, 10);
+//     i = 0;
+//     while (i < 9)
+//     {
+//         printf("%d\n", range[i]);
+//         i++;
+//     }
+//     return (0);
+// }
