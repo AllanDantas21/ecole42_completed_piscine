b8b85ca22f330f73e4a7141152f876b796128ec9
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-09 02:34:56 -0300

    init c9

---
+++ c08/ex00/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 02:31:56 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/09 02:34:41 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
