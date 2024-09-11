a84f2d47cf9fb709d138db3c3bdc2bf0b95c93c7
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-10 23:15:19 -0300

    add ex01/c09 -> makefile

---
+++ c09/ex01/includes/ ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    ft.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 23:06:18 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/10 23:09:29 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
