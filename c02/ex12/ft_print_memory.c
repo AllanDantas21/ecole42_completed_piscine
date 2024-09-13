a9ba585a8582b2d552b7304b687671a64220bbdf
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-12 22:28:48 -0300

    fix ft_print_memory

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldantas <aldantas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 00:56:52 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/12 16:43:56 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/12 22:25:29 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,22 @@ void	ft_print_hex(unsigned char c)
 void	ft_print_address(unsigned long long addr)
 {
 	const char	*hex = "0123456789abcdef";
+	char		address[16];
+	int			i;
 
-	if (addr >= 16)
-		ft_print_address(addr / 16);
-	write(1, &hex[addr % 16], 1);
+	i = 15;
+	while (i >= 0)
+	{
+		address[i] = hex[addr % 16];
+		addr /= 16;
+		i--;
+	}
+	i = 0;
+	while (address[i] != '\0')
+	{
+		write(1, &address[i], 1);
+		i++;
+	}
 }
 
 void	ft_hex_content(unsigned char *memory, unsigned int i,
@@ -71,9 +83,11 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	unsigned int	i;
 	unsigned char	*memory;
 
+	if (size == 1)
+		return (addr);
 	i = 0;
 	memory = (unsigned char *)addr;
-	while (i < size)
+	while (i <= size)
 	{
 		ft_print_address((unsigned long long)(memory + i));
 		write(1, ": ", 2);
@@ -82,12 +96,14 @@ void	*ft_print_memory(void *addr, unsigned int size)
 		write(1, "\n", 1);
 		i += 16;
 	}
+	write(1, "\n", 1);
 	return (addr);
 }
-
+/*
+#include <stdio.h>
 int main() {
 	char str[] = "Bonjour les aminches\t\n\tc'est fou\tce qu'on \
 	peutfaire avec\t\n\tprint_memory\n\nlol.lol\n ";
 	ft_print_memory(str, sizeof(str));
 	return 0;
-}
+}*/
