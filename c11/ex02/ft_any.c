e05248c77d6d86af3743ab6f85e1a496008d59bf
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-16 00:04:42 -0300

    add ft_any & ft_count_if

--- c11/ex02/ft_any.c
+++ c11/ex02/ft_any.c
@@ -6,7 +6,41 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 23:47:49 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/15 23:47:55 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/15 23:54:06 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_any(char **tab, int(*f)(char*))
+{
+	int i;
+
+	i = 0;
+	while(tab[i])
+	{
+		if (f(tab[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*
+#include <stdio.h>
+
+int ft_is_upper(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 'A' && *str <= 'Z')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char *tab[5] = {"hello", "world", "42", "RIO", 0};
+	printf("%d\n", ft_any(tab, &ft_is_upper));
+	return (0);
+}*/
--- c11/ex03/ft_count_if.c
+++ c11/ex03/ft_count_if.c
@@ -6,7 +6,43 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 23:33:39 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/15 23:33:39 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/16 00:04:15 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_count_if(char **tab, int length, int (*f)(char*))
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (i < length)
+	{
+		if (f(tab[i]))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+/*
+#include <stdio.h>
+
+int ft_is_upper(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 'A' && *str <= 'Z')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char *tab[5] = {"hello", "WORLD", "42", "RIO", "CPP"};
+	printf("%d\n", ft_count_if(tab, 5, &ft_is_upper));
+	return (0);
+}*/
