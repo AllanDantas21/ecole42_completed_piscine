b363a07821b1d94ff359567ab816470e54deab8f
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-17 02:08:07 -0300

    add ft_sort_string

---
+++ c11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 02:00:32 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/17 02:07:01 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	swap_str(char **a, char **b)
+{
+	char *temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = i + 1;
+		while (tab[j])
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+				swap_str(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+#include <stdio.h>
+int main()
+{
+	char *tab[] = {"abc", "cat", "bde", 0};
+	ft_sort_string_tab(tab);
+	int i = 0;
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	return 0;
+}
