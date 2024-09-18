866cd7e87b8847999a77e068a61e0029bd8b2a8a
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-18 00:19:50 -0300

    refact ft_tail -> split handle_op & no_op

--- c10/ex02/src/ft_tail.c
+++ c10/ex02/src/ft_tail.c
@@ -6,39 +6,13 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:51:10 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/18 00:01:32 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/18 00:17:52 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tail.h"
-#include "stdio.h"
-	/* -c dá handle dos ultimos n bytes do arquivo 
-	bytes++; -> conta todos os bytes do arquivo, se tive -c de flag
-	vou separa a count lines;
-	*/
 
-static int	count_lines(char *file_name)
-{
-	int		fd;
-	char	buffer[1];
-	int		count;
-
-	count = 0;
-	fd = open(file_name, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	while (read(fd, buffer, 1))
-	{
-		if (*buffer == '\n')
-			count++;
-	}
-	close(fd);
-	if (count > 10)
-		return (count - 10);
-	return (0);
-}
-
-static int	open_file(char *file_name)
+int	open_file(char *file_name)
 {
 	int	fd;
 
@@ -54,51 +28,12 @@ static int	open_file(char *file_name)
 	return (fd);
 }
 
-static void	ft_print_lines(int fd, int first_line)
-{
-	char	buffer[1];
-	int		count;
-	int		ret;
-
-	count = 0;
-	ret = 1;
-	while (ret != 0)
-	{
-		ret = read(fd, buffer, 1);
-		if (ret == -1)
-		{
-			ft_error(strerror(errno));
-			return ;
-		}
-		if (ret != 0)
-		{
-			if (count >= first_line)
-				write(1, buffer, 1);
-			if (*buffer == '\n')
-				count++;
-		}
-	}
-}
-
 int	ft_cat(char *file_name, int	option)
 {
-	int		fd;
-	int		first_line;
-	int		first_bytes;
-
-	//fd = open(file_name, O_RDONLY);
-	fd = open_file(file_name);
-	if (!fd)
-		return (0);
-	if (!option)
-		first_line = count_lines(file_name);
-	else if (option)
-		first_bytes = count_bytes(file_name, option);
-	if (!option)
-		ft_print_lines(fd, first_line);
 	if (option)
-		ft_print_bytes(fd, first_bytes);
-	close(fd);
+		tail_with_option(file_name, option);
+	if (!option)
+		tail_no_option(file_name);
 	return (1);
 }
 
--- c10/ex02/src/no_option.c
+++ c10/ex02/src/no_option.c
@@ -1,40 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_option.c                                    :+:      :+:    :+:   */
+/*   no_option.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 23:56:28 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/18 00:19:11 by aldantas         ###   ########.fr       */
+/*   Created: 2024/09/18 00:16:39 by aldantas          #+#    #+#             */
+/*   Updated: 2024/09/18 00:19:07 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tail.h"
 
-static int count_bytes(char *file_name, int option)
+static int  count_lines(char *file_name)
 {
 	int		fd;
 	char	buffer[1];
-	int		bytes;
+	int		count;
 
-	bytes = 0;
+	count = 0;
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	while (read(fd, buffer, 1))
-		bytes++;
+	{
+		if (*buffer == '\n')
+			count++;
+	}
 	close(fd);
-	return (bytes - option);
+	if (count > 10)
+		return (count - 10);
+	return (0);
 }
 
-static void	ft_print_bytes(int fd, int first_bytes)
+static void ft_print_lines(int fd, int first_line)
 {
 	char	buffer[1];
-	int		bytes;
+	int		count;
 	int		ret;
 
-	bytes = 0;
+	count = 0;
 	ret = 1;
 	while (ret != 0)
 	{
@@ -46,23 +51,24 @@ static void	ft_print_bytes(int fd, int first_bytes)
 		}
 		if (ret != 0)
 		{
-			if (bytes >= first_bytes)
+			if (count >= first_line)
 				write(1, buffer, 1);
-            bytes++;
+			if (*buffer == '\n')
+				count++;
 		}
 	}
 }
 
-void    tail_with_option(char *file_name, int option)
+void    tail_no_option(char *file_name)
 {
-	int		first_bytes;
-    int     fd;
-
-	fd = open_file(file_name);
-	if (!fd)
+		int	first_line;
+		int	fd;
+		
+		fd = open_file(file_name);
+		if (!fd)
+			return ;
+		first_line = count_lines(file_name);
+		ft_print_lines(fd, first_line);
+		close(fd);
 		return ;
-	first_bytes = count_bytes(file_name, option);
-	ft_print_bytes(fd, first_bytes);
-	close(fd);
-	return ;
 }
