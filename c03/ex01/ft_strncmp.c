6177021e6a470079cc2f3d0eb9981894c65d0180
aldantas <allandantas21@gmail.com>
2024-09-22 15:11:41 -0300

    fix strncmp

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -3,19 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*   By: aldantas <aldantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:30:56 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/19 14:52:11 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/22 15:11:23 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int				result;
 	unsigned int	i;
 
-	result = 0;
 	i = 0;
 	while ((s1[i] != '\0' && i < n) || (s2[i] != '\0' && i < n))
 	{
