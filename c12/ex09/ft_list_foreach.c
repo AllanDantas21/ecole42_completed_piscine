9310b349a4769dd4116ffc7335fe6e5acc095a4f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-20 15:05:48 -0300

    add ft_list_foreach

---
+++ c12/ex09/ft_list_foreach.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 15:02:22 by aldantas          #+#    #+#             */
+/*   Updated: 2024/11/20 15:05:36 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    while(begin_list)
+    {
+        f(begin_list->data);
+        begin_list = begin_list->next;
+    }
+}
+
+// ******* TEST MAIN *******
+// #include <stdio.h>
+// void print_data(void *data)
+// {
+//     printf("%s\n", (char *)data);
+// }
+
+// int main()
+// {
+//     t_list node3 = { "Node 3", NULL };
+//     t_list node2 = { "Node 2", &node3 };
+//     t_list node1 = { "Node 1", &node2 };
+
+//     ft_list_foreach(&node1, print_data);
+//     return (0);
+// }
