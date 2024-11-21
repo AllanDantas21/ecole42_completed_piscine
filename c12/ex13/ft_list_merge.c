d9d21168248cd313cff4c7a953440be4814ea50f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-21 10:06:32 -0300

    fix norms

--- c12/ex13/ft_list_merge.c
+++ c12/ex13/ft_list_merge.c
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:49:04 by aldantas          #+#    #+#             */
-/*   Updated: 2024/11/21 09:56:13 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/11/21 10:06:08 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void ft_list_merge(t_list **begin_list1, t_list *begin_list2)
 {
-    t_list *tmp = *begin_list1;
+    t_list *tmp;
 
+    tmp = *begin_list1;
     while (tmp->next)
         tmp = tmp->next;
     tmp->next = begin_list2;
