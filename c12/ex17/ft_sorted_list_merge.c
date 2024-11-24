8e8f14981fdb48f88af8a301190cc38f7ee6ec2a
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-24 18:55:19 -0300

    comment main test

--- c12/ex17/ft_sorted_list_merge.c
+++ c12/ex17/ft_sorted_list_merge.c
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:49:58 by aldantas          #+#    #+#             */
-/*   Updated: 2024/11/24 18:52:46 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/11/24 18:54:59 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,58 +58,59 @@ static void ft_list_sort(t_list **begin_list, int (*cmp)())
     }
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// TEST 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-t_list *ft_create_elem(void *data)
-{
-    t_list *new_elem;
+// t_list *ft_create_elem(void *data)
+// {
+//     t_list *new_elem;
 
-    new_elem = (t_list *)malloc(sizeof(t_list));
-    if (new_elem)
-    {
-        new_elem->data = data;
-        new_elem->next = NULL;
-    }
-    return new_elem;
-}
+//     new_elem = (t_list *)malloc(sizeof(t_list));
+//     if (new_elem)
+//     {
+//         new_elem->data = data;
+//         new_elem->next = NULL;
+//     }
+//     return new_elem;
+// }
 
-int cmp(void *a, void *b)
-{
-    return strcmp((char *)a, (char *)b);
-}
+// int cmp(void *a, void *b)
+// {
+//     return strcmp((char *)a, (char *)b);
+// }
 
-void print_list(t_list *list)
-{
-    while (list)
-    {
-        printf("%s -> ", (char *)list->data);
-        list = list->next;
-    }
-    printf("NULL\n");
-}
+// void print_list(t_list *list)
+// {
+//     while (list)
+//     {
+//         printf("%s -> ", (char *)list->data);
+//         list = list->next;
+//     }
+//     printf("NULL\n");
+// }
 
-int main()
-{
-    t_list *list1 = ft_create_elem("apple");
-    list1->next = ft_create_elem("banana");
-    list1->next->next = ft_create_elem("cherry");
+// int main()
+// {
+//     t_list *list1 = ft_create_elem("apple");
+//     list1->next = ft_create_elem("banana");
+//     list1->next->next = ft_create_elem("cherry");
 
-    t_list *list2 = ft_create_elem("apricot");
-    list2->next = ft_create_elem("blueberry");
-    list2->next->next = ft_create_elem("citrus");
+//     t_list *list2 = ft_create_elem("apricot");
+//     list2->next = ft_create_elem("blueberry");
+//     list2->next->next = ft_create_elem("citrus");
 
-    printf("List 1 before merge and sort:\n");
-    print_list(list1);
+//     printf("List 1 before merge and sort:\n");
+//     print_list(list1);
 
-    printf("List 2 before merge and sort:\n");
-    print_list(list2);
+//     printf("List 2 before merge and sort:\n");
+//     print_list(list2);
 
-    ft_sorted_list_merge(&list1, list2, cmp);
+//     ft_sorted_list_merge(&list1, list2, cmp);
 
-    printf("List 1 after merge and sort:\n");
-    print_list(list1);
+//     printf("List 1 after merge and sort:\n");
+//     print_list(list1);
 
-    return 0;
-}
+//     return 0;
+// }
