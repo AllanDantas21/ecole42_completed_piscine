f14db1a3648341dd78c43d4351cf819dc927e836
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-24 18:47:14 -0300

    add ft_sorted_list

--- c12/ex16/ft_sorted_list_insert.c
+++ c12/ex16/ft_sorted_list_insert.c
@@ -6,11 +6,20 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:22:05 by aldantas          #+#    #+#             */
-/*   Updated: 2024/11/24 18:24:27 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/11/24 18:46:48 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+
+static void ft_list_sort(t_list **begin_list, int (*cmp)());
+static void ft_list_push_front(t_list **begin_list, void *data);
+
+void ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+{
+    ft_list_push_front(begin_list, data);
+    ft_list_sort(begin_list, cmp);
+}
 
 static void ft_list_sort(t_list **begin_list, int (*cmp)())
 {
@@ -39,7 +48,52 @@ static void ft_list_sort(t_list **begin_list, int (*cmp)())
     }
 }
 
-void ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+static void ft_list_push_front(t_list **begin_list, void *data)
 {
-    
+    t_list *node;
+
+    if (!data)
+        return;
+    node = (t_list *)malloc(sizeof(t_list));
+    if (node)
+    {
+        node->data = data;
+        node->next = *begin_list;
+        *begin_list = node;
+    }
 }
+
+// TEST MAIN
+// #include <stdio.h>
+// #include <string.h>
+
+// int cmp(void *a, void *b)
+// {
+//     return strcmp((char *)a, (char *)b);
+// }
+
+// int main(void)
+// {
+//     t_list *list = NULL;
+
+//     ft_sorted_list_insert(&list, "banana", cmp);
+//     ft_sorted_list_insert(&list, "apple", cmp);
+//     ft_sorted_list_insert(&list, "cherry", cmp);
+
+//     t_list *current = list;
+//     while (current)
+//     {
+//         printf("%s\n", (char *)current->data);
+//         current = current->next;
+//     }
+
+//     // Free the list
+//     while (list)
+//     {
+//         t_list *temp = list;
+//         list = list->next;
+//         free(temp);
+//     }
+
+//     return 0;
+// }
