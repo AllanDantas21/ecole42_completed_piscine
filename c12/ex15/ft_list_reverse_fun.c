b1a6487146a6a08a2217dcc513c110c72bcb311f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-24 18:25:30 -0300

    add ex15

---
+++ c12/ex15/ft_list_reverse_fun.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse_fun.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 18:13:48 by aldantas          #+#    #+#             */
+/*   Updated: 2024/11/24 18:25:11 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void ft_list_reverse_fun(t_list *begin_list)
+{
+    t_list *curr;
+    t_list *next;
+    void *temp;
+
+    if (!begin_list || !begin_list->next)
+        return;
+
+    curr = begin_list;
+    while (curr)
+    {
+        next = curr->next;
+        while (next)
+        {
+            temp = curr->data;
+            curr->data = next->data;
+            next->data = temp;
+            next = next->next;
+        }
+        curr = curr->next;
+    }
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// t_list *create_elem(void *data)
+// {
+//     t_list *new_elem = (t_list *)malloc(sizeof(t_list));
+//     if (new_elem)
+//     {
+//         new_elem->data = data;
+//         new_elem->next = NULL;
+//     }
+//     return new_elem;
+// }
+
+// void print_list(t_list *list)
+// {
+//     while (list)
+//     {
+//         printf("%s ", (char *)list->data);
+//         list = list->next;
+//     }
+//     printf("\n");
+// }
+
+// int main()
+// {
+//     t_list *list = create_elem("one");
+//     list->next = create_elem("two");
+//     list->next->next = create_elem("three");
+//     list->next->next->next = create_elem("four");
+
+//     printf("Original list:\n");
+//     print_list(list);
+
+//     ft_list_reverse_fun(list);
+
+//     printf("Reversed list:\n");
+//     print_list(list);
+
+//     return 0;
+// }
---
+++ c12/ex16/ft_sorted_list_insert.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sorted_list_insert.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 18:22:05 by aldantas          #+#    #+#             */
+/*   Updated: 2024/11/24 18:24:27 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+static void ft_list_sort(t_list **begin_list, int (*cmp)())
+{
+    t_list *i;
+    t_list *j;
+    void *temp;
+
+    if (!begin_list || !*begin_list)
+        return;
+
+    i = *begin_list;
+    while (i)
+    {
+        j = i->next;
+        while (j)
+        {
+            if ((*cmp)(i->data, j->data) > 0)
+            {
+                temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+            j = j->next;
+        }
+        i = i->next;
+    }
+}
+
+void ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+{
+    
+}
