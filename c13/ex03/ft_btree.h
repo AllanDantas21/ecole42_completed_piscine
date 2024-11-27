4015bf820a8bc3e01a7df74fe8c907a6a3db0ef7
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-27 10:40:55 -0300

    init ex1/2/3

---
+++ c13/ex03/ft_btree.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 19:00:44 by aldantas          #+#    #+#             */
+/*   Updated: 2024/11/27 10:40:36 by aldantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+typedef struct s_btree
+{
+    struct s_btree  *left;
+    struct s_btree  *right;
+    void            *item;
+}   t_btree;
+
+#endif
