7529e688989c2e78e2a95173ba5e647da3e6629d
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-01 12:01:20 -0300

    adjust ft_hexdump handle

--- c10/ex03/src/handle_option.c
+++ c10/ex03/src/handle_option.c
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:49:04 by aldantas          #+#    #+#             */
-/*   Updated: 2024/11/30 16:02:47 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/12/01 12:00:07 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 static void	ft_print_address(unsigned long addr)
 {
 	const char	*hex = "0123456789abcdef";
-	char		address[16];
+	char		address[8];
 	int			i;
 
-	i = 15;
+	i = 7;
 	while (i >= 0)
 	{
 		address[i] = hex[addr % 16];
@@ -26,12 +26,12 @@ static void	ft_print_address(unsigned long addr)
 		i--;
 	}
 	i = 0;
-	while (i < 16)
+	while (i < 8)
 	{
 		write(1, &address[i], 1);
 		i++;
 	}
-	write(1, ": ", 2);
+	write(1, "  ", 2);
 }
 
 static void	ft_hex_content(unsigned char *addr, unsigned int limit)
@@ -46,14 +46,14 @@ static void	ft_hex_content(unsigned char *addr, unsigned int limit)
 		{
 			write(1, &hex[addr[i] / 16], 1);
 			write(1, &hex[addr[i] % 16], 1);
-            write(1, " ", 1);
+			if (i == 7)
+				write(1, "  ", 2);
+			else
+				write(1, " ", 1);
 		}
-		else
-			write(1, "  ", 2);
-		if (i % 2 != 0)
-			write(1, " ", 1);
 		i++;
 	}
+	write(1, " ", 1);
 }
 
 static void	ft_print_chars(unsigned char *addr, unsigned int limit)
@@ -82,7 +82,7 @@ static unsigned int	adjust_limit(unsigned int i, unsigned int size)
 
 void	ft_hexdump_opt(char *file_name, unsigned int size)
 {
-	char            addr[BUFFER_SIZE * 2];
+	char            addr[BUFFER_SIZE];
     int             fd;
 	unsigned int    i;
 	unsigned int    limit;
@@ -96,10 +96,11 @@ void	ft_hexdump_opt(char *file_name, unsigned int size)
 	{
 		read(fd, addr, BUFFER_SIZE);
 		limit = adjust_limit(i, size);
-		ft_print_address((unsigned long)addr + i);
-		ft_hex_content((unsigned char *)addr + i, limit);
-		ft_print_chars((unsigned char *)addr + i, limit);
+		ft_print_address((unsigned long)i);
+		ft_hex_content((unsigned char *)addr, limit);
+		ft_print_chars((unsigned char *)addr, limit);
 		write(1, "\n", 1);
-		i += BUFFER_SIZE;
+		i += limit;
 	}
+	close(fd);
 }
