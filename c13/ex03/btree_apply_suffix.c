d12234fbe5eaf06f45c30820e02aa197b1730c98
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-03 15:37:12 -0300

    add apply suffix

--- c13/ex03/btree_apply_suffix.c
+++ c13/ex03/btree_apply_suffix.c
@@ -1,56 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   btree_apply_infix.c                                :+:      :+:    :+:   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 12:47:02 by aldantas          #+#    #+#             */
-/*   Updated: 2024/12/03 13:01:10 by aldantas         ###   ########.fr       */
+/*   Created: 2024/12/03 13:01:38 by aldantas          #+#    #+#             */
+/*   Updated: 2024/12/03 15:36:57 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
 
-void btree_apply_infix(t_btree *root, void (*applyf)(void *))
+void btree_apply_suffix(t_btree *root, void (*applyf)(void *))
 {
     if (!root)
         return ;
-    btree_apply_infix(root->left, applyf);
+    btree_apply_suffix(root->left, applyf);
+    btree_apply_suffix(root->right, applyf);
     applyf(root->item);
-    btree_apply_infix(root->right, applyf);
 }
 
-/*
+/*  TEST
 #include <stdio.h>
 #include <stdlib.h>
-t_btree *btree_create_node(void *item)
-{
-    t_btree *node;
-
-    node = (t_btree *)malloc(sizeof(t_btree));
-    if (!node)
-        return NULL;
-    node->item = item;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
 
 void print_item(void *item)
 {
     printf("%s\n", (char *)item);
 }
 
+t_btree *btree_create_node(void *item)
+{
+    t_btree *node = (t_btree *)malloc(sizeof(t_btree));
+    if (node)
+    {
+        node->left = NULL;
+        node->right = NULL;
+        node->item = item;
+    }
+    return node;
+}
+
 int main(void)
 {
-    t_btree *root;
-
-    root = btree_create_node("root");
+    t_btree *root = btree_create_node("root");
     root->left = btree_create_node("left");
     root->right = btree_create_node("right");
     root->left->left = btree_create_node("left->left");
     root->left->right = btree_create_node("left->right");
-    btree_apply_infix(root, print_item);
+    btree_apply_suffix(root, print_item);
     return 0;
 }*/
