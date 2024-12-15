9daea632fbafbf66668583f96b068eecc8f5a7be
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-14 23:49:20 -0300

    comments

--- c13/ex05/btree_search_item.c
+++ c13/ex05/btree_search_item.c
@@ -29,68 +29,68 @@ void *btree_search_item(t_btree *root, void *data_ref, int (*cmpf)(void *, void 
     return (0);
 }
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
 
-t_btree *btree_create_node(void *item)
-{
-    t_btree *new_node;
+// t_btree *btree_create_node(void *item)
+// {
+//     t_btree *new_node;
 
-    new_node = (t_btree *)malloc(sizeof(t_btree));
-    if (new_node)
-    {
-        new_node->item = item;
-        new_node->left = NULL;
-        new_node->right = NULL;
-    }
-    return (new_node);
-}
+//     new_node = (t_btree *)malloc(sizeof(t_btree));
+//     if (new_node)
+//     {
+//         new_node->item = item;
+//         new_node->left = NULL;
+//         new_node->right = NULL;
+//     }
+//     return (new_node);
+// }
 
-void btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
-{
-    if (*root == NULL)
-        *root = btree_create_node(item);
-    else if (cmpf(item, (*root)->item) < 0)
-        btree_insert_data(&(*root)->left, item, cmpf);
-    else
-        btree_insert_data(&(*root)->right, item, cmpf);
-}
+// void btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
+// {
+//     if (*root == NULL)
+//         *root = btree_create_node(item);
+//     else if (cmpf(item, (*root)->item) < 0)
+//         btree_insert_data(&(*root)->left, item, cmpf);
+//     else
+//         btree_insert_data(&(*root)->right, item, cmpf);
+// }
 
-void btree_free(t_btree *root)
-{
-    if (root)
-    {
-        btree_free(root->left);
-        btree_free(root->right);
-        free(root);
-    }
-}
+// void btree_free(t_btree *root)
+// {
+//     if (root)
+//     {
+//         btree_free(root->left);
+//         btree_free(root->right);
+//         free(root);
+//     }
+// }
 
-int cmpf(void *a, void *b)
-{
-    return (strcmp((char *)a, (char *)b));
-}
+// int cmpf(void *a, void *b)
+// {
+//     return (strcmp((char *)a, (char *)b));
+// }
 
-int main(void)
-{
-    t_btree *root;
-    char *data_ref = "search_item";
-    char *result;
+// int main(void)
+// {
+//     t_btree *root;
+//     char *data_ref = "search_item";
+//     char *result;
 
-    // Assume btree_create_node and btree_insert_data are implemented
-    root = btree_create_node("root_item");
-    btree_insert_data(&root, "left_item", cmpf);
-    btree_insert_data(&root, "right_item", cmpf);
-    btree_insert_data(&root, "search_item", cmpf);
+//     // Assume btree_create_node and btree_insert_data are implemented
+//     root = btree_create_node("root_item");
+//     btree_insert_data(&root, "left_item", cmpf);
+//     btree_insert_data(&root, "right_item", cmpf);
+//     btree_insert_data(&root, "search_item", cmpf);
 
-    result = (char *)btree_search_item(root, data_ref, cmpf);
-    if (result)
-        printf("Item found: %s\n", result);
-    else
-        printf("Item not found\n");
+//     result = (char *)btree_search_item(root, data_ref, cmpf);
+//     if (result)
+//         printf("Item found: %s\n", result);
+//     else
+//         printf("Item not found\n");
 
-    // Assume btree_free is implemented to free the tree
-    btree_free(root);
-    return (0);
-}
+//     // Assume btree_free is implemented to free the tree
+//     btree_free(root);
+//     return (0);
+// }
