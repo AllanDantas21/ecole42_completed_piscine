27933f5283a64e486ab7104ede8e34026e0fdcfa
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-15 22:11:54 -0300

    atts

--- c13/ex06/btree_level_count.c
+++ c13/ex06/btree_level_count.c
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 21:59:26 by aldantas          #+#    #+#             */
-/*   Updated: 2024/12/15 22:10:56 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/12/15 22:11:41 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,31 @@ int tree_level_count(t_btree *root)
         return (right + 1);
 }
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// t_btree *btree_create_node(void *item)
-// {
-//     t_btree *new_node;
+/*
+#include <stdio.h>
+#include <stdlib.h>
+t_btree *btree_create_node(void *item)
+{
+    t_btree *new_node;
 
-//     new_node = (t_btree *)malloc(sizeof(t_btree));
-//     if (new_node)
-//     {
-//         new_node->item = item;
-//         new_node->left = NULL;
-//         new_node->right = NULL;
-//     }
-//     return (new_node);
-// }
-// int main(void)
-// {
-//     t_btree *root;
-//     root = btree_create_node("42");
-//     root->left = btree_create_node("21");
-//     root->right = btree_create_node("84");
-//     root->left->left = btree_create_node("10");
-//     root->left->right = btree_create_node("30");
-//     printf("Tree Level Count: %d\n", tree_level_count(root));
-//     return (0);
-// }
+    new_node = (t_btree *)malloc(sizeof(t_btree));
+    if (new_node)
+    {
+        new_node->item = item;
+        new_node->left = NULL;
+        new_node->right = NULL;
+    }
+    return (new_node);
+}
+int main(void)
+{
+    t_btree *root;
+    root = btree_create_node("42");
+    root->left = btree_create_node("21");
+    root->right = btree_create_node("84");
+    root->left->left = btree_create_node("10");
+    root->left->right = btree_create_node("30");
+    printf("Tree Level Count: %d\n", tree_level_count(root));
+    return (0);
+}
+*/
