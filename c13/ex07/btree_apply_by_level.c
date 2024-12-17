257c84d4aea1d4a63c0d388fddc0a81407d82251
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-16 22:24:52 -0300

    add func

--- c13/ex07/btree_apply_by_level.c
+++ c13/ex07/btree_apply_by_level.c
@@ -6,9 +6,13 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 22:03:10 by aldantas          #+#    #+#             */
-/*   Updated: 2024/12/15 22:11:28 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/12/16 22:14:41 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_btree.h"
 
+void btree_apply_by_level(t_btree *root, void (*applyf)(void *item, int current_level, int is_first_elem))
+{
+    
+}
