7ac77203f308df909591fa0212e91c80ef60acde
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-25 21:40:35 -0300

    att c10

--- c13/ex07/btree_apply_by_level.c
+++ c13/ex07/btree_apply_by_level.c
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 22:03:10 by aldantas          #+#    #+#             */
-/*   Updated: 2024/12/25 21:12:47 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/12/25 21:37:01 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void btree_apply_by_level(t_btree *root, void (*applyf)(void *item, int current_level, int is_first_elem))
 {
-    
+    int level;
 }
