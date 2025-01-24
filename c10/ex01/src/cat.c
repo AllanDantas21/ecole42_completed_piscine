eb4abeeee566d09b093e17104c682410ea93cc3e
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-01-23 21:40:59 -0300

    improve cat

--- c10/ex01/src/cat.c
+++ c10/ex01/src/cat.c
@@ -12,11 +12,15 @@
 
 #include "cat.h"
 
-void	ft_error(char *err)
+void	ft_error(char *filename, char *err)
 {
+	write(2, "cat: ", 5);
+	while (*filename)
+		write(2, filename++, 1);
+	write(2, ": ", 2);
 	while (*err)
 		write(2, err++, 1);
-	write(1, "\n", 1);
+	write(2, "\n", 1);
 }
 
 int	ft_display_file(char *file_name)
@@ -29,33 +33,44 @@ int	ft_display_file(char *file_name)
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 	{
-		ft_error(strerror(errno));
-		return (0);
+		ft_error(file_name, strerror(errno));
+		return (1);
 	}
 	while (ret != 0)
 	{
 		ret = read(fd, buffer, 1);
 		if (ret == -1)
 		{
-			ft_error(strerror(errno));
-			return (0);
+			ft_error(file_name, strerror(errno));
+			close(fd);
+			return (1);
 		}
 		if (ret != 0)
 			write(1, buffer, 1);
 	}
 	close(fd);
-	return (1);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	int	i;
+	int		i;
+	int		exit_status;
+	char	buffer[1];
 
+	exit_status = 0;
+	if (argc == 1)
+	{
+		while (read(0, buffer, 1) > 0)
+			write(1, buffer, 1);
+		return (0);
+	}
 	i = 1;
 	while (i < argc)
 	{
-		ft_display_file(argv[i]);
+		if (ft_display_file(argv[i]) != 0)
+			exit_status = 1;
 		i++;
 	}
-	return (0);
+	return (exit_status);
 }
